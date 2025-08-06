0b5dd7eb89fa7798a443f2f0ecd119d0580cd4f2
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2025-08-06 23:10:17 +0300

    db context ve tüm entityler ekledi servis kaydı yapıldı

---
+++ Infrastructure/TaskProjectManagementApi.Persistence/AppDbContext/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskProjectManagementApi.Domain.Entity;
+
+namespace TaskProjectManagementApi.Persistence.AppDbContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Company> companies { get; set; }
+        public DbSet<CompanyAuthor> companiesAuthor { get; set; }
+        public DbSet<IndividualMissions> individualMissions { get; set; }
+        public DbSet<IndividualMissionsAnswer>  IndividualMissionsAnswers { get; set; }
+        public DbSet<MissionStatus> missionStatuses { get; set; }
+        public DbSet<Team> teams { get; set; }
+        public DbSet<TeamMissions> teamMissions { get; set; }
+        public DbSet<TeamMissionsAnswer> teamMissionsAnswers { get; set; }
+        public DbSet<Worker> workers { get; set; }
+
+
+    }
+}
---
+++ Presentation/TaskProjectManagementApi.Api/Extensions/ServicesExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskProjectManagementApi.Persistence.AppDbContext;
+
+namespace TaskProjectManagementApi.Api.Extensions
+{
+    public static class ServicesExtensions
+    {
+        public static void AddServer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
