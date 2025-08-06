5bb9927f10e2aa6d27feeb21d52dae13c9587a70
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2025-08-06 23:35:51 +0300

    READ VE WRİTE OPERASYONLARI AYRILIP UNİTOFWORK YAPISI IMPLEMENTE EDİLDİ

---
+++ Core/TaskProjectManagementApi.Application/Interfaces/IWriteRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskProjectManagementApi.Domain.Common;
+
+namespace TaskProjectManagementApi.Application.Interfaces
+{
+    public interface IWriteRepository<T> : IBaseEntity
+    {
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ Infrastructure/TaskProjectManagementApi.Persistence/Registration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskProjectManagementApi.Application.Interfaces;
+using TaskProjectManagementApi.Application.Interfaces.UnitOfWork;
+using TaskProjectManagementApi.Persistence.AppDbContext;
+using TaskProjectManagementApi.Persistence.Repositories;
+using TaskProjectManagementApi.Persistence.UnitOfWorks;
+
+namespace TaskProjectManagementApi.Persistence
+{
+    public static class Registration
+    {
+        public static void ConfigurePersistence(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
+            services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+
+        }
+    }
+}
